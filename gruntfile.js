479c0184ea3d85e2fa29f6e36273ab8871ed46d3
egardner <egardner@ITTEMPs-Mac-Pro.local>
2014-08-08 12:00:22 -0700

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                        'js/libs/*.js',     // all JS in the libs folder
+                        'js/global.js'
+                ],
+                dest: 'js/build/production.js', 
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images'
+                }]
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+
+            css: {
+                files: ['css/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+
+        connect: {
+            client: {
+                options: {
+                    port: 9000,
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/build/global.css': 'css/global.scss'
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['sass', 'concat', 'uglify', 'imagemin']);
+    grunt.registerTask('preview', ['connect', 'watch']);
+
+};
